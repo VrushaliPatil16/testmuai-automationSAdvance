4a5b3c6e8412dd317b951e0d67d759c265f4cbbb
VrushaliPatil16 <129206095+VrushaliPatil16@users.noreply.github.com>
2026-02-16 18:30:55 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,13 +1,18 @@
 package base;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -16,14 +21,35 @@ public class BaseTest {
     String username = "patilvb22nov21";
     String accessKey = "LT_BCIbGZ3M8ofU65bM9pEEnq3hZKuVHd9DMfTt7Qqfu3hnoRO";
 
-    @BeforeMethod
-    public void setup() throws Exception {
+    @Parameters({"browser", "browserVersion", "platformName", "testName"})
+    @BeforeMethod(alwaysRun = true)
+    public void setup(String browser,
+                      String browserVersion,
+                      String platformName,
+                      String testName) throws Exception {
 
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("browserName", "Chrome");
-        options.setCapability("browserVersion", "latest");
-        options.setCapability("platformName", "Windows 11");
-        options.setCapability("name", "Selenium Advanced Assignment");
+        MutableCapabilities options;
+
+        if (browser.equalsIgnoreCase("Chrome")) {
+            options = new ChromeOptions();
+        } else {
+            options = new EdgeOptions();
+        }
+
+        options.setCapability("browserName", browser);
+        options.setCapability("browserVersion", browserVersion);
+        options.setCapability("platformName", platformName);
+
+        // TestMu AI specific capabilities
+        Map<String, Object> testmuOptions = new HashMap<>();
+        testmuOptions.put("name", testName);
+        testmuOptions.put("build", "Selenium Advanced Assignment");
+        testmuOptions.put("networkLogs", true);
+        testmuOptions.put("consoleLogs", true);
+        testmuOptions.put("video", true);
+        testmuOptions.put("screenshots", true);
+
+        options.setCapability("testmu:options", testmuOptions);
 
         String hubURL = "https://" + username + ":" + accessKey +
                 "@hub.testmuai.com/wd/hub";
@@ -33,7 +59,7 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         if (driver != null) {
             driver.quit();
